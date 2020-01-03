cdddc1a409cddde20b489e58e4db8c3fe8a9095b
wangzhenglongcn <49703920+wangzhenglongcn@users.noreply.github.com>
2020-01-04 00:31:41 +0800

    测试

--- src/main/java/config/WebConfigurer.java
+++ src/main/java/config/WebConfigurer.java
@@ -28,7 +28,7 @@ public class WebConfigurer implements WebMvcConfigurer {
 
     // 这个方法用来注册拦截器，我们自己写好的拦截器需要通过这里添加注册才能生效
     @Override
-    public void addInterceptors(InterceptorRegistry registry) {
+    public void addInterceptors(InterceptorRegistry registry ) {
         /*registry.addInterceptor(loginInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/static/**","/","/index","/dashboard");*/
