0b20b461388485e8fcb2bf2a529e950d2eb121b8
wangzhenglongcn <49703920+wangzhenglongcn@users.noreply.github.com>
2020-04-30 17:29:52 +0800

    接口 工具类

--- src/main/java/controller/Index.java
+++ src/main/java/controller/Index.java
@@ -3,6 +3,8 @@ package controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +12,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import util.EncryptUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
 
 
 @Controller
@@ -50,5 +57,49 @@ public class Index {
     public String test(@PathVariable int id,HttpServletRequest request){
         request.setAttribute("key",UserMapper.selectUserById(id).toString());
         return "/show";
+    }
+
+    /**
+     * http  向我发送请求的接口
+     * @return 发送的数据
+     * @throws IOException
+     */
+    @RequestMapping(value = "sendTask")
+    public @ResponseBody
+    JSONObject sendTask(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        EncryptUtils.getInstance();
+        String requestString;
+        String token = request.getHeader("token");
+        requestString = getBodyString(request.getReader());
+        JSONObject jsonObjects = JSONObject.parseObject(requestString);
+        if(jsonObjects.size() > 0){
+            JSONObject dataObject2 = new JSONObject();
+            dataObject2.put("state",404);
+            dataObject2.put("total",0);
+            response.setContentType("application/Json");
+            try {
+                response.getWriter().write(JSONObject.toJSONString(dataObject2));
+                response.getWriter().flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return  jsonObjects;
+        }
+        return null;
+    }
+
+
+    public String getBodyString(BufferedReader br) {
+        String inputLine;
+        String str = "";
+        try {
+            while ((inputLine = br.readLine()) != null) {
+                str += inputLine;
+            }
+            br.close();
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+        return str;
     }
 }
